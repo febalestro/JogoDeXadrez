9d218e081f033d8925769c32f405650937849445
Fernanda Balestro <febalestro@gmail.com>
2022-07-24 16:37:32 -0300

    Melhorando a implementação do método ImprimirTabuleiro

--- Jogodexadrez/Program.cs
+++ Jogodexadrez/Program.cs
@@ -1,8 +1,21 @@
 ﻿using tabuleiro;
 using Jogodexadrez;
 using xadrez;
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
+    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+   // tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+   // tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
-PosicaoXadrez pos = new PosicaoXadrez('c', 1);
+    tab.ColocarPeca(new Torre(Cor.Branca, tab), new Posicao(3, 5));
+    tab.ColocarPeca(new Torre(Cor.Branca, tab), new Posicao(3, 6));
+    tab.ColocarPeca(new Rei(Cor.Branca, tab), new Posicao(3, 7));
 
-Console.WriteLine(pos.ToPosicao());
+    Tela.ImprimirTabuleiro(tab);
+}
+catch (TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
